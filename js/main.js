ca41f01f66181689100956be9e66b4bcff780836
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2021-02-11 14:17:18 +0100

    Finishing the NavBar and Starting the Intro Section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+/* ====== Show and Close Menu ====== */
+const toggleMenu = document.querySelector(".toggle-menu");
+const headerContainer = document.querySelector(".header-container");
+
+toggleMenu.addEventListener("click", () => {
+  headerContainer.classList.toggle("open");
+});
